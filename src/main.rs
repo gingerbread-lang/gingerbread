a771d6f9a0f782b937f883aac307f545e301bab9
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-08-24 11:04:44 +1000

    Add initial lexer skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod lexer;
+
+use std::io;
+
+fn main() -> anyhow::Result<()> {
+    let stdin = io::stdin();
+    let mut input = String::new();
+
+    loop {
+        stdin.read_line(&mut input)?;
+
+        let tokens = lexer::lex(&input);
+        dbg!(tokens.collect::<Vec<_>>());
+
+        input.clear();
+    }
 }
