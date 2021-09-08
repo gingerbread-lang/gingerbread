846e1c80d9d61bd525c3798dfb03717f1ca0ba16
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-09-08 18:08:33 +1000

    Add AST validation and evaluation to fuzzer

--- unnamedc/fuzz/fuzz_targets/main.rs
+++ unnamedc/fuzz/fuzz_targets/main.rs
@@ -7,5 +7,7 @@ fuzz_target!(|s: &str| {
     let tokens = lexer::lex(s);
     let parse = parser::parse(tokens);
     let root = ast::Root::cast(parse.syntax_node()).unwrap();
-    let _program = hir_lower::lower(&root);
+    let _validation_errors = ast::validation::validate(&root);
+    let program = hir_lower::lower(&root);
+    let _result = eval::eval(program);
 });
