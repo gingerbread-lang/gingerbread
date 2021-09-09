8a7a04ac58823292eeef58f991724ee7b9661a03
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-09-09 22:37:20 +1000

    Include type inference in fuzzing

--- unnamedc/fuzz/fuzz_targets/main.rs
+++ unnamedc/fuzz/fuzz_targets/main.rs
@@ -8,7 +8,8 @@ fuzz_target!(|s: &str| {
     let parse = parser::parse(tokens);
     let root = ast::Root::cast(parse.syntax_node()).unwrap();
     let _validation_errors = ast::validation::validate(&root);
-    let program = hir_lower::lower(&root);
+    let (program, _source_map) = hir_lower::lower(&root);
+    let _infer_result = hir_ty::infer(&program);
     let mut evaluator = eval::Evaluator::default();
     let _result = evaluator.eval(program);
 });
