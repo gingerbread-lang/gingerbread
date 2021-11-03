4f07aa5c31459bbc052fe68ef47fb390eda49db0
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-11-03 11:14:23 +1100

    Fix fuzzing and update starting corpus

--- unnamedc/fuzz/fuzz_targets/main.rs
+++ unnamedc/fuzz/fuzz_targets/main.rs
@@ -5,7 +5,7 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|s: &str| {
     let tokens = lexer::lex(s);
-    let parse = parser::parse(&tokens);
+    let parse = parser::parse_repl_line(&tokens);
     let root = ast::Root::cast(parse.syntax_node()).unwrap();
     let _validation_errors = ast::validation::validate(&root);
     let (program, _source_map, _lower_errors, _local_def_names) = hir_lower::lower(&root);
