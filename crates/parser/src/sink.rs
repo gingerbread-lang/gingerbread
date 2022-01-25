e55a93565386fc9dd80df227e56e8d6f632bbbdc
Luna Razzaghipour <aramisnoah@gmail.com>
2022-01-25 22:08:03 +1100

    Replace index-based loop with pointer math in syntax tree construction

--- crates/parser/src/sink.rs
+++ crates/parser/src/sink.rs
@@ -18,8 +18,8 @@ impl<'tokens, 'input> Sink<'tokens, 'input> {
     pub(crate) fn finish(mut self, errors: Vec<SyntaxError>) -> Parse {
         // the first event always starts the root node,
         // and the last event always finishes that node
-        debug_assert!(matches!(self.events[0], Event::StartNode { .. }));
-        debug_assert!(matches!(self.events[self.events.len() - 1], Event::FinishNode));
+        assert!(matches!(self.events.get(0), Some(Event::StartNode { .. })));
+        assert!(matches!(self.events.last(), Some(Event::FinishNode)));
 
         // We want to avoid nodes having trailing trivia:
         //
@@ -34,28 +34,36 @@ impl<'tokens, 'input> Sink<'tokens, 'input> {
         // 1 * 2 + 3
         // ^^^^^^
 
-        // we go through all event indices apart from the last one,
+        // we go through all events apart from the last one,
         // since we can’t peek what the next event is when we’re at the last
         // and thus need to handle it specially
-        for event_idx in 0..self.events.len() - 1 {
-            self.process_event(event_idx);
 
-            let next_event = &self.events[event_idx + 1];
-            match next_event {
+        let mut current = self.events.as_ptr();
+        let mut next = unsafe { current.add(1) };
+        let last = &self.events[self.events.len() - 1] as *const _;
+
+        while current != last {
+            self.process_event(unsafe { *current });
+
+            match unsafe { *next } {
                 Event::StartNode { .. } | Event::AddToken => self.skip_trivia(),
                 Event::FinishNode => {}
             }
+
+            current = next;
+            next = unsafe { current.add(1) };
         }
+
         // unconditionally skip any trivia before processing the last event
         // to ensure we don’t miss trailing trivia at the end of the input
         self.skip_trivia();
-        self.process_event(self.events.len() - 1);
+        self.process_event(unsafe { *last });
 
         Parse { syntax_node: self.builder.finish(), errors }
     }
 
-    fn process_event(&mut self, idx: usize) {
-        match self.events[idx] {
+    fn process_event(&mut self, event: Event) {
+        match event {
             Event::StartNode { kind } => self.builder.start_node(kind),
             Event::FinishNode => self.builder.finish_node(),
             Event::AddToken => self.add_token(),
