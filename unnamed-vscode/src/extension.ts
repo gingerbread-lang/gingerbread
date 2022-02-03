23a0f257d88b0dd8f73c57ae4262c01bb29cdc3b
Luna Razzaghipour <aramisnoah@gmail.com>
2022-02-03 14:43:28 +1100

    Create bare-bones VS Code language configuration

--- unnamed-vscode/src/extension.ts
+++ unnamed-vscode/src/extension.ts
@@ -15,7 +15,7 @@ export function activate(context: ExtensionContext) {
   };
 
   const clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: "file", language: "plaintext" }],
+    documentSelector: [{ scheme: "file", language: "unnamed" }],
   };
 
   client = new LanguageClient(
