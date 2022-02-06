89f95be2569de0b589c3965eaef85942e00d415c
Luna Razzaghipour <aramisnoah@gmail.com>
2022-02-06 14:45:42 +1100

    Extract multi-module text shorthand into `utils` crate

--- crates/hir/src/body.rs
+++ crates/hir/src/body.rs
@@ -572,7 +572,7 @@ mod tests {
         expect: Expect,
         expected_diagnostics: [(LoweringDiagnosticKind, std::ops::Range<u32>); N],
     ) {
-        let modules = split_multi_module_test_data(input);
+        let modules = utils::split_multi_module_test_data(input);
         let mut world_index = WorldIndex::default();
 
         for (name, text) in &modules {
@@ -606,43 +606,6 @@ mod tests {
             .collect();
 
         assert_eq!(expected_diagnostics, actual_diagnostics);
-    }
-
-    fn split_multi_module_test_data(input: &str) -> HashMap<&str, &str> {
-        const MARKER_COMMENT_START: &str = "#- ";
-
-        let has_no_marker_comments = !input.contains(MARKER_COMMENT_START);
-        if has_no_marker_comments {
-            let mut modules = HashMap::with_capacity(1);
-            modules.insert("main", input);
-            return modules;
-        }
-
-        let mut module_idxs = HashMap::with_capacity(1);
-        let mut current_module_name = None;
-        let mut line_idxs = input.match_indices('\n').map(|(idx, _)| idx + 1).peekable();
-
-        while let Some(line_start) = line_idxs.next() {
-            let line_end = match line_idxs.peek() {
-                Some(end) => *end,
-                None => break,
-            };
-
-            let line = &input[line_start..line_end];
-            if let Some(idx) = line.find(MARKER_COMMENT_START) {
-                let module_name_start = idx + MARKER_COMMENT_START.len();
-                let module_name_end = line.len() - 1; // remove newline
-
-                let module_name = &line[module_name_start..module_name_end];
-
-                module_idxs.insert(module_name, line_end..line_end);
-                current_module_name = Some(module_name);
-            }
-
-            module_idxs.get_mut(&current_module_name.unwrap()).unwrap().end = line_end;
-        }
-
-        module_idxs.into_iter().map(|(module_name, range)| (module_name, &input[range])).collect()
     }
 
     #[test]
---
+++ crates/utils/src/lib.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+pub fn split_multi_module_test_data(input: &str) -> HashMap<&str, &str> {
+    const MARKER_COMMENT_START: &str = "#- ";
+
+    let has_no_marker_comments = !input.contains(MARKER_COMMENT_START);
+    if has_no_marker_comments {
+        let mut modules = HashMap::with_capacity(1);
+        modules.insert("main", input);
+        return modules;
+    }
+
+    let mut module_idxs = HashMap::with_capacity(1);
+    let mut current_module_name = None;
+    let mut line_idxs = input.match_indices('\n').map(|(idx, _)| idx + 1).peekable();
+
+    while let Some(line_start) = line_idxs.next() {
+        let line_end = match line_idxs.peek() {
+            Some(end) => *end,
+            None => break,
+        };
+
+        let line = &input[line_start..line_end];
+        if let Some(idx) = line.find(MARKER_COMMENT_START) {
+            let module_name_start = idx + MARKER_COMMENT_START.len();
+            let module_name_end = line.len() - 1; // remove newline
+
+            let module_name = &line[module_name_start..module_name_end];
+
+            module_idxs.insert(module_name, line_end..line_end);
+            current_module_name = Some(module_name);
+        }
+
+        module_idxs.get_mut(&current_module_name.unwrap()).unwrap().end = line_end;
+    }
+
+    module_idxs.into_iter().map(|(module_name, range)| (module_name, &input[range])).collect()
+}
