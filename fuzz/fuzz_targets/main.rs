9861298a92c25a19c28ab23014c1cb6b8519c2aa
Luna Razzaghipour <aramisnoah@gmail.com>
2022-03-07 17:30:53 +1100

    Update fuzzing

--- fuzz/fuzz_targets/main.rs
+++ fuzz/fuzz_targets/main.rs
@@ -4,10 +4,13 @@ use ast::AstNode;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|s: &str| {
+    let world_index = hir::WorldIndex::default();
+
     let tokens = lexer::lex(s);
     let parse = parser::parse_repl_line(&tokens);
     let root = ast::Root::cast(parse.syntax_node()).unwrap();
-    let _validation_errors = ast::validation::validate(&root);
-    let lower_result = hir_lower::lower(&root);
-    let _infer_result = hir_ty::infer(&lower_result.program);
+    let _diagnostics = ast::validation::validate(&root);
+    let (index, _diagnostics) = hir::index(&root, &world_index);
+    let (bodies, _diagnostics) = hir::lower(&root, &index, &world_index);
+    let (_inference, _diagnostics) = hir_ty::infer_all(&bodies, &index, &world_index);
 });
