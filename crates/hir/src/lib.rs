e6b8fac6594923cdfae32c12eb10b677888e8cce
Luna Razzaghipour <aramisnoah@gmail.com>
2022-03-08 17:00:55 +1100

    Qualify all submodule imports with `self::`

--- crates/hir/src/lib.rs
+++ crates/hir/src/lib.rs
@@ -3,7 +3,7 @@ mod index;
 mod index_diff;
 mod world_index;
 
-pub use body::*;
-pub use index::*;
-pub use index_diff::*;
-pub use world_index::*;
+pub use self::body::*;
+pub use self::index::*;
+pub use self::index_diff::*;
+pub use self::world_index::*;
