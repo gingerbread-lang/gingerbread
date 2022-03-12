be0c1f76e06cc2a2ac71e2e454293e0e3c41fbe8
Luna Razzaghipour <aramisnoah@gmail.com>
2022-03-12 17:23:18 +1100

    Add memory usage instrumentation to benchmark

--- crates/bench/src/main.rs
+++ crates/bench/src/main.rs
@@ -1,8 +1,39 @@
 use ast::AstNode;
+use std::alloc::GlobalAlloc;
 use std::env;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 #[global_allocator]
-static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+static GLOBAL: TrackingAlloc =
+    TrackingAlloc { total_size: AtomicUsize::new(0), mimalloc: mimalloc::MiMalloc };
+
+struct TrackingAlloc {
+    total_size: AtomicUsize,
+    mimalloc: mimalloc::MiMalloc,
+}
+
+unsafe impl GlobalAlloc for TrackingAlloc {
+    unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
+        self.total_size.fetch_add(layout.size(), Ordering::SeqCst);
+        self.mimalloc.alloc(layout)
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: std::alloc::Layout) {
+        self.total_size.fetch_sub(layout.size(), Ordering::SeqCst);
+        self.mimalloc.dealloc(ptr, layout)
+    }
+
+    unsafe fn alloc_zeroed(&self, layout: std::alloc::Layout) -> *mut u8 {
+        self.total_size.fetch_add(layout.size(), Ordering::SeqCst);
+        self.mimalloc.alloc_zeroed(layout)
+    }
+
+    unsafe fn realloc(&self, ptr: *mut u8, layout: std::alloc::Layout, new_size: usize) -> *mut u8 {
+        self.total_size.fetch_sub(layout.size(), Ordering::SeqCst);
+        self.total_size.fetch_add(new_size, Ordering::SeqCst);
+        self.mimalloc.realloc(ptr, layout, new_size)
+    }
+}
 
 fn main() {
     let mut args = env::args();
@@ -12,7 +43,7 @@ fn main() {
         Some("parser") => {
             let input = gen::gen(4 << 10 << 10); // 4 MiB
             let tokens = lexer::lex(&input);
-            parser::parse_source_file(&tokens);
+            parser::parse_source_file(&tokens, &input);
         }
 
         Some("short") => {
@@ -22,11 +53,12 @@ fn main() {
                         fnc add(x: s32, y: s32): s32 -> x + y;
                         fnc main -> add 10, 20 + 30;
                     "#,
+                    false,
                 );
             }
         }
 
-        Some("long") => compile(&gen::gen(16 << 10 << 10)), // 16 MiB
+        Some("long") => compile(&gen::gen(16 << 10 << 10), true), // 16 MiB
 
         Some(_) => eprintln!("Unrecognized benchmark name"),
 
@@ -34,13 +66,27 @@ fn main() {
     }
 }
 
-fn compile(input: &str) {
+fn compile(input: &str, should_print: bool) {
+    mem_usage("base", should_print);
     let world_index = hir::WorldIndex::default();
     let tokens = lexer::lex(input);
+    mem_usage("lexed", should_print);
     let tree = parser::parse_repl_line(&tokens, input).into_syntax_tree();
+    mem_usage("parsed", should_print);
     let root = ast::Root::cast(tree.root(), &tree).unwrap();
+    mem_usage("got ast", should_print);
     let _diagnostics = ast::validation::validate(root, &tree);
+    mem_usage("validated", should_print);
     let (index, _diagnostics) = hir::index(root, &tree, &world_index);
+    mem_usage("indexed", should_print);
     let (bodies, _diagnostics) = hir::lower(root, &tree, &index, &world_index);
+    mem_usage("lowered", should_print);
     let (_inference, _diagnostics) = hir_ty::infer_all(&bodies, &index, &world_index);
+    mem_usage("inferred", should_print);
+}
+
+fn mem_usage(stage: &str, should_print: bool) {
+    if should_print {
+        println!("{stage:>20}: {}MB", GLOBAL.total_size.load(Ordering::SeqCst) / 1_000_000);
+    }
 }
